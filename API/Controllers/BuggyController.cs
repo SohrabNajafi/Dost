f9f3275d4f9801fcec5b674a5b6a787e3dcd7ed2
SohrabNajafi <sohrab.najafi.93@gmail.com>
2021-09-19 18:58:57 +0430

    Repository_Automapper_SedingData

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -24,7 +24,7 @@ namespace API.Controllers
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
-            var thing = _context.Users.Find(Convert.ToInt64(-1));
+            var thing = _context.Users.Find(-1);
 
             if (thing == null) return NotFound();
 
